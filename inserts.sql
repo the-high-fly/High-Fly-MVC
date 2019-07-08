4cbfa5937d79a5dcfe7b0071667ad8691557aeb9
rob93c <rob.uniud@gmail.com>
2019-07-08 18:03:03 +0200

    Inserts added (temp)

---
+++ inserts.sql
@@ -0,0 +1,32 @@
+insert into cliente
+values (1, 1, 'rob', 'aiuto');
+insert into cliente
+values (2, 1, 'fabio', 'bellosql');
+insert into cliente
+values (3, 1, 'mario', 'funghetti');
+insert into cliente
+values (4, 0, 'trivago', 'viaggio20');
+insert into cliente
+values (5, 0, 'username', 'password'); -- Golden Tour
+insert into cliente
+values (6, 1, 'ludo', 'vica');
+insert into cliente
+values (7, 1, 'raffa', 'ellissima');
+insert into cliente
+values (8, 0, 'MSC', 'naviii');
+insert into cliente
+values (9, 0, 'Italo', 'trenirossi');
+
+insert into veicolo_privato
+values (1, 5, 100, 'Macchina');
+insert into veicolo_privato
+values (2, 4, 80, 'Macchina');
+insert into veicolo_privato
+values (3, 2, 50, 'Moto');
+insert into veicolo_privato
+values (4, 2, 70, 'Moto');
+insert into veicolo_privato
+values (5, 1, 30, 'Bicicletta');
+insert into veicolo_privato
+values (6, 1, 20, 'Bicicletta');
+
