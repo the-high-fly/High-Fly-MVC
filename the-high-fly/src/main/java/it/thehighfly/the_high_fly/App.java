f282c0ce954ac84aed961f52b4ea8cfc8e8e2976
rob93c <rob.uniud@gmail.com>
2019-07-08 15:06:42 +0200

    Setup

---
+++ the-high-fly/src/main/java/it/thehighfly/the_high_fly/App.java
@@ -0,0 +1,13 @@
+package it.thehighfly.the_high_fly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
