d9423556584dfc29fb81725bd17e5da9b075ad5a
rob93c <rob.uniud@gmail.com>
2019-07-10 15:08:23 +0200

    RestController updated

--- the-high-fly/src/main/java/it/thehighfly/the_high_fly/controller/VeicoloController.java
+++ the-high-fly/src/main/java/it/thehighfly/the_high_fly/controller/VeicoloController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class VeicoloController {
 	@Autowired
 	private VeicoloService veicoloService;
 	
-	@RequestMapping(value = "/listaVeicoli/", method = RequestMethod.GET)
+	@RequestMapping(value = "/veicoli/", method = RequestMethod.GET)
     public ResponseEntity<ArrayList<VeicoloVo>> listVehicles() {
     	ArrayList<VeicoloVo> listaVeicoli;
     	
@@ -33,5 +34,21 @@ public class VeicoloController {
     		e.printStackTrace();
     		return new ResponseEntity<ArrayList<VeicoloVo>>(HttpStatus.INTERNAL_SERVER_ERROR);
     	}
+	}
+	
+	@RequestMapping(value = "/veicoli/{id}", method = RequestMethod.GET)
+	public ResponseEntity<VeicoloVo> getVehicle(@PathVariable("id") int id) {
+		VeicoloVo veicolo;
+		try {
+			veicolo = veicoloService.retVeicolo(id);
+			if (veicolo == null) {
+				System.out.println("Veicolo " + id + " non trovato");
+				return new ResponseEntity<VeicoloVo>(HttpStatus.NOT_FOUND);
+			}
+			return new ResponseEntity<VeicoloVo>(veicolo, HttpStatus.OK);
+		} catch(Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<VeicoloVo>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
